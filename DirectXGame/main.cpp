2ce7a5cde4cc3a0ff4507d4423cfd91dcc07e249
mutoairi <146906672+mutoairi@users.noreply.github.com>
2025-04-17 10:28:04 +0900

    完成

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,44 +1,42 @@
+#include "GameScene.h"
+#include <KamataEngine.h>
 #include <Windows.h>
-#include<KamataEngine.h>
-#include"GameScene.h"
-
 
 using namespace KamataEngine;
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	//エンジンの初期化
+	// エンジンの初期化
 	KamataEngine::Initialize(L"LE3C_25_ムトウ_アイリ_CG4");
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
-    //ゲームシーンの初期化
-	GameScene* gameScene =new GameScene();
+	// ゲームシーンの初期化
+	GameScene* gameScene = new GameScene();
 	gameScene->Initialize();
-	    //メインループ
+	// メインループ
 	while (true) {
-	
-	//エンジンの更新
+
+		// エンジンの更新
 		if (KamataEngine::Update()) {
-		break;
+			break;
 		}
-		//ゲームシーンのアップデート
+		// ゲームシーンのアップデート
 		gameScene->Update();
 
-	//描画開始
+		// 描画開始
 		dxCommon->PreDraw();
 
-	//ゲームシーンの描画
+		// ゲームシーンの描画
 		gameScene->Draw();
 
-
-	//描画終了
+		// 描画終了
 		dxCommon->PostDraw();
 	}
-	//ゲームシーンの解放
+	// ゲームシーンの解放
 	delete gameScene;
-	//nullptrの代入
+	// nullptrの代入
 	gameScene = nullptr;
 
-	//エンジンの終了処理
+	// エンジンの終了処理
 	KamataEngine::Finalize();
 	return 0;
 }
