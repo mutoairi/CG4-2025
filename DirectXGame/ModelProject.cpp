9daf64691a9ea9053f9273e1909a7f93c85a3a87
mutoairi <146906672+mutoairi@users.noreply.github.com>
2025-08-01 00:47:31 +0900

    hennkou

--- DirectXGame/ModelProject.cpp
+++ DirectXGame/ModelProject.cpp
@@ -148,19 +148,19 @@ ModelProject* ModelProject::CreateSquare() {
 	/*左下*/
 	vertices[0].pos = {-0.5f, -0.5f, 0.0f};
 	vertices[0].uv = {0.0f, 1.0f};
-	vertices[0].normal = {0.0f, 0.0f, 1.0f};
+	vertices[0].normal = {0.0f, 0.0f, -1.0f};
 	/*左上*/
 	vertices[1].pos = {-0.5f, 0.5f, 0.0f};
 	vertices[1].uv = {0.0f, 0.0f};
-	vertices[1].normal = {0.0f, 0.0f, 1.0f};
+	vertices[1].normal = {0.0f, 0.0f, -1.0f};
 	/*右下*/
 	vertices[2].pos = {0.5f, -0.5f, 0.0f};
 	vertices[2].uv = {1.0f, 1.0f};
-	vertices[2].normal = {0.0f, 0.0f, 1.0f};
+	vertices[2].normal = {0.0f, 0.0f, -1.0f};
 	/*右上*/
 	vertices[3].pos = {0.5f, 0.5f, 0.0f};
 	vertices[3].uv = {1.0f, 0.0f};
-	vertices[3].normal = {0.0f, 0.0f, 1.0f};
+	vertices[3].normal = {0.0f, 0.0f, -1.0f};
 
 	indices[0] = 0;
 	indices[1] = 1;
